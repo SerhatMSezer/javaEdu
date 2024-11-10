cbb2d9e9fab6f630b547b2917ed569c7d194cd4e
SerhatMSezer <hayatadonus777@gmail.com>
2024-11-10 23:29:37 +0300

    Switch case exmp

---
+++ SwitchCase/src/SwitchCaseExample01.java
@@ -0,0 +1,29 @@
+public class SwitchCaseExample01 {
+    public static void main(String[] args) {
+        int number=15;
+        switch(number)
+        {
+          case 1:
+              System.out.println("First number is 1");
+              break;
+              case 2:
+                  System.out.println("Second number is 2");
+                  break;
+                  case 3:
+                      System.out.println("Third number is 3");
+                      break;
+                      case 4:
+                          System.out.println("Fourth number is 4");
+                          break;
+                          case 5:
+                              System.out.println("Fifth number is 5");
+                              break;
+                              case 6:
+                                  System.out.println("Sixth number is 6");
+                                  break;
+                                  default:
+                                      System.out.println("Invalid number");
+                                      break;
+        }
+    }
+}
